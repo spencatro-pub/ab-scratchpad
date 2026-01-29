a1f6a943bf1593a5000666ed01259cd2f3c49d53
Spencer Hawins (mesa) <shawkins-mesa@shawk.in>
2026-01-29 12:46:00 -0800

    init

--- src/main.py
+++ src/main.py
@@ -3,12 +3,32 @@ import argparse
 
 def main():
     # this line was written by a human
-    parser = argparse.ArgumentParser(description='Generate a QR code for google.com')
+    parser = argparse.ArgumentParser(description='Generate a QR code')
+    parser.add_argument(
+        '--data',
+        default='https://google.com',
+        help='URL or data to encode (default: https://google.com)'
+    )
     parser.add_argument(
         '--ecc',
         choices=['L', 'M', 'Q', 'H'],
         default='H',
         help='Error correction level: L (7%%), M (15%%), Q (25%%), H (30%%) (default: H)'
+    )
+    parser.add_argument(
+        '--output',
+        default='qrcode.png',
+        help='Output filename (default: qrcode.png)'
+    )
+    parser.add_argument(
+        '--fill-color',
+        default='black',
+        help='Fill/foreground color (default: black)'
+    )
+    parser.add_argument(
+        '--back-color',
+        default='white',
+        help='Background color (default: white)'
     )
     args = parser.parse_args()
     
@@ -21,12 +41,12 @@ def main():
     
     # this one too
     qr = qrcode.QRCode(error_correction=ecc_map[args.ecc])
-    qr.add_data('https://google.com')
+    qr.add_data(args.data)
     qr.make()
     
-    img = qr.make_image(fill_color='black', back_color='white')
-    img.save('qrcode.png')
-    print(f'QR code generated with ECC level {args.ecc} and saved to qrcode.png')
+    img = qr.make_image(fill_color=args.fill_color, back_color=args.back_color)
+    img.save(args.output)
+    print(f'QR code generated with ECC level {args.ecc} and saved to {args.output}')
 
 if __name__ == '__main__':
     main()
